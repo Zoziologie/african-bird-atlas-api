f15c041fd21edaba32b29cd17b5b729159aca1d9
Raphaël Nussbaumer <rafnuss@gmail.com>
2021-03-27 07:28:35 +0100

    adapt to new website and pentad selector visibility

--- script.js
+++ script.js
@@ -1,67 +1,70 @@
-
 //import location from './location.json'
 
 
 var markersLayer, app
 var now = new Date
-const res = 5/60;
+const res = 5 / 60;
 
 /* PREPARE FOR COOKIES*/
 var later = new Date();
-later.setTime(later.getTime() + (365*24*60*60*1000));
-var expires = "expires="+ later.toUTCString();
-cookies_input=['base','regionType','regionCountry','regionPentad','regionGroup','regionQdgc','filterType','filterObserver','speciesCode','startdate','enddate','cardNo'];
+later.setTime(later.getTime() + (365 * 24 * 60 * 60 * 1000));
+var expires = "expires=" + later.toUTCString();
+cookies_input = ['base', 'regionType', 'regionCountry', 'regionPentad', 'regionGroup', 'regionQdgc', 'filterType', 'filterObserver', 'speciesCode', 'startdate', 'enddate', 'cardNo'];
 
 function getCookieValue(a) {
 	var b = document.cookie.match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)');
 	return b ? decodeURIComponent(b.pop()) : '';
 }
 
-function pentad2latlng(p){
-	lat0 = parseFloat(p.slice(0,2)) + parseFloat(p.slice(2,4)/60)
-	lng0 = parseFloat(p.slice(5,7)) + parseFloat(p.slice(7,10)/60)
-	if (p[4]=="_" | p[4]=="a"){
+function pentad2latlng(p) {
+	lat0 = parseFloat(p.slice(0, 2)) + parseFloat(p.slice(2, 4) / 60)
+	lng0 = parseFloat(p.slice(5, 7)) + parseFloat(p.slice(7, 10) / 60)
+	if (p[4] == "_" | p[4] == "a") {
 		lat0 = -lat0;
 	}
-	if (p[4]=="b" | p[4]=="a"){
+	if (p[4] == "b" | p[4] == "a") {
 		lng0 = -lng0;
-	} 
+	}
 	return [lat0, lng0]
 }
 
-function latlng2pentad(lat,lng){
-	if (lat < 0 & lng > 0){
+function latlng2pentad(lat, lng) {
+	if (lat < 0 & lng > 0) {
 		letter = '_'
 	} else if (lat < 0 & lng < 0) {
 		letter = 'a'
-	} else if (lat > 0  & lng < 0) {
+	} else if (lat > 0 & lng < 0) {
 		letter = 'b'
 	} else if (lat > 0 & lng > 0) {
 		letter = 'c'
 	}
-	
-	var  latstr = (Math.floor(Math.abs(lat+0.0001)/res)*res*100).toLocaleString(undefined,{ 
-		minimumIntegerDigits: 4, useGrouping: false, maximumFractionDigits:0 
+
+	var latstr = (Math.floor(Math.abs(lat + 0.0001) / res) * res * 100).toLocaleString(undefined, {
+		minimumIntegerDigits: 4,
+		useGrouping: false,
+		maximumFractionDigits: 0
 	})
-	var  lngstr = (Math.floor(Math.abs(lng+0.0001)/res)*res*100).toLocaleString(undefined,{ 
-		minimumIntegerDigits: 4, useGrouping: false, maximumFractionDigits:0 
+	var lngstr = (Math.floor(Math.abs(lng + 0.0001) / res) * res * 100).toLocaleString(undefined, {
+		minimumIntegerDigits: 4,
+		useGrouping: false,
+		maximumFractionDigits: 0
 	})
-	
-	return latstr+letter+lngstr
-	
+
+	return latstr + letter + lngstr
+
 }
 
 window.onload = function () {
-	
+
 	Vue.component('v-select', VueSelect.VueSelect);
-	
+
 	Vue.component('l-map', window.Vue2Leaflet.LMap);
 	Vue.component('l-tile-layer', window.Vue2Leaflet.LTileLayer)
 	Vue.component('l-rectangle', window.Vue2Leaflet.LRectangle)
 	Vue.component('l-marker', window.Vue2Leaflet.LMarker)
 	Vue.component('l-geo-json', window.Vue2Leaflet.LGeoJson)
-	
-	
+
+
 	app = new Vue({
 		delimiters: ["((", "))"],
 		el: '#ff',
@@ -81,19 +84,161 @@ window.onload = function () {
 			regionProvince: "",
 			format: "json",
 			first: "",
-			groups: [ {label:"Aberdare Range", code: "40147_AbrdrRng"}, {label:"Amboseli Landscape", code: "40147_AmbslLndscp"}, {label:"Arabuko-Sokoke Forest", code: "40147_Arbk-SkkFrst"}, {label:"Athi-Kaputiei Plains", code: "40147_Ath-KptPlns"}, {label:"Boni-Dodori Reserves", code: "40147_Bn-DrdRsrvs"}, {label:"Cherangani Hills", code: "40147_ChrngnHlls"}, {label:"Chyulu Hills", code: "40147_ChylHlls"}, {label:"Coast - North", code: "40147_NrthCst-KE"}, {label:"Coast - South", code: "40147_SthCst-KE"}, {label:"Dida-Galgalu Desert", code: "40147_Dd-GlglDsrt"}, {label:"Gwasi Hills", code: "40147_GwsHlls"}, {label:"Kakamega and Nandi Forests", code: "40147_KkmgndNndFrsts"}, {label:"Laikipia", code: "40147_Lkp"}, {label:"Lake Baringo", code: "40147_LkBrng"}, {label:"Lake Bogoria", code: "40147_LkBgr"}, {label:"Lake Elementaita", code: "40147_LkElmntt"}, {label:"Lake Magadi", code: "40147_LkMgd"}, {label:"Lake Naivasha", code: "40147_LkNvsh"}, {label:"Lake Nakuru", code: "40147_LkNkr"}, {label:"Lake Ol Bolossat", code: "40147_LkOlBlsst"}, {label:"Lake Turkana", code: "40147_LkTrkn"}, {label:"Lake Victoria", code: "40147_LkVctr"}, {label:"Loima Hills", code: "40147_LmHlls"}, {label:"Loita-Nguruman Hills", code: "40147_Lt-NgrmnHlls"}, {label:"Lorian Swamp", code: "40147_LrnSwmp"}, {label:"Masai Mara", code: "40147_GrtrMsMr"}, {label:"Matthews Range", code: "40147_MtthwsRng"}, {label:"Mau Forest Complex", code: "40147_MFrstCmplx"}, {label:"Meru-Kora-Rahole-Bisanadi Reserves", code: "40147_Mr-Kr-Rhl-BsndRsrvs"}, {label:"Mount Elgon", code: "40147_MntElgn-KE"}, {label:"Mount Kenya", code: "40147_MntKny"}, {label:"Mount Kulal", code: "40147_MntKll"}, {label:"Mount Marsabit", code: "40147_MntMrsbt"}, {label:"Mount Nyiru", code: "40147_MntNyr"}, {label:"Mount Suswa", code: "40147_MntSsw"}, {label:"Nairobi", code: "40147_Nrb"}, {label:"Ndoto Mountains", code: "40147_NdtMntns"}, {label:"Ngong Hills", code: "40147_NgngHlls"}, {label:"North-Eastern", code: "40147_Nrth-Estrn"}, {label:"Nyambene Hills", code: "40147_NymbnHlls"}, {label:"Sekerr Range (Mtelo)", code: "40147_SkrrRng(Mtl)"}, {label:"Shimba Hills", code: "40147_ShmbHlls"}, {label:"Tana River Delta", code: "40147_TnRvrDlt"}, {label:"Tana River Mid Section", code: "40147_TnRvrMddlSctn"}, {label:"Tsavo Parks", code: "40147_TsvEstndWst"}, {label:"Turkwel River", code: "40147_TrkwlRvr"} ],			regions: ['country','pentad','province','group','qdgc'],
-			regions_opt: ['all','country','pentad','province','group','qdgc'],
+			groups: [{
+				label: "Aberdare Range",
+				code: "40147_AbrdrRng"
+			}, {
+				label: "Amboseli Landscape",
+				code: "40147_AmbslLndscp"
+			}, {
+				label: "Arabuko-Sokoke Forest",
+				code: "40147_Arbk-SkkFrst"
+			}, {
+				label: "Athi-Kaputiei Plains",
+				code: "40147_Ath-KptPlns"
+			}, {
+				label: "Boni-Dodori Reserves",
+				code: "40147_Bn-DrdRsrvs"
+			}, {
+				label: "Cherangani Hills",
+				code: "40147_ChrngnHlls"
+			}, {
+				label: "Chyulu Hills",
+				code: "40147_ChylHlls"
+			}, {
+				label: "Coast - North",
+				code: "40147_NrthCst-KE"
+			}, {
+				label: "Coast - South",
+				code: "40147_SthCst-KE"
+			}, {
+				label: "Dida-Galgalu Desert",
+				code: "40147_Dd-GlglDsrt"
+			}, {
+				label: "Gwasi Hills",
+				code: "40147_GwsHlls"
+			}, {
+				label: "Kakamega and Nandi Forests",
+				code: "40147_KkmgndNndFrsts"
+			}, {
+				label: "Laikipia",
+				code: "40147_Lkp"
+			}, {
+				label: "Lake Baringo",
+				code: "40147_LkBrng"
+			}, {
+				label: "Lake Bogoria",
+				code: "40147_LkBgr"
+			}, {
+				label: "Lake Elementaita",
+				code: "40147_LkElmntt"
+			}, {
+				label: "Lake Magadi",
+				code: "40147_LkMgd"
+			}, {
+				label: "Lake Naivasha",
+				code: "40147_LkNvsh"
+			}, {
+				label: "Lake Nakuru",
+				code: "40147_LkNkr"
+			}, {
+				label: "Lake Ol Bolossat",
+				code: "40147_LkOlBlsst"
+			}, {
+				label: "Lake Turkana",
+				code: "40147_LkTrkn"
+			}, {
+				label: "Lake Victoria",
+				code: "40147_LkVctr"
+			}, {
+				label: "Loima Hills",
+				code: "40147_LmHlls"
+			}, {
+				label: "Loita-Nguruman Hills",
+				code: "40147_Lt-NgrmnHlls"
+			}, {
+				label: "Lorian Swamp",
+				code: "40147_LrnSwmp"
+			}, {
+				label: "Masai Mara",
+				code: "40147_GrtrMsMr"
+			}, {
+				label: "Matthews Range",
+				code: "40147_MtthwsRng"
+			}, {
+				label: "Mau Forest Complex",
+				code: "40147_MFrstCmplx"
+			}, {
+				label: "Meru-Kora-Rahole-Bisanadi Reserves",
+				code: "40147_Mr-Kr-Rhl-BsndRsrvs"
+			}, {
+				label: "Mount Elgon",
+				code: "40147_MntElgn-KE"
+			}, {
+				label: "Mount Kenya",
+				code: "40147_MntKny"
+			}, {
+				label: "Mount Kulal",
+				code: "40147_MntKll"
+			}, {
+				label: "Mount Marsabit",
+				code: "40147_MntMrsbt"
+			}, {
+				label: "Mount Nyiru",
+				code: "40147_MntNyr"
+			}, {
+				label: "Mount Suswa",
+				code: "40147_MntSsw"
+			}, {
+				label: "Nairobi",
+				code: "40147_Nrb"
+			}, {
+				label: "Ndoto Mountains",
+				code: "40147_NdtMntns"
+			}, {
+				label: "Ngong Hills",
+				code: "40147_NgngHlls"
+			}, {
+				label: "North-Eastern",
+				code: "40147_Nrth-Estrn"
+			}, {
+				label: "Nyambene Hills",
+				code: "40147_NymbnHlls"
+			}, {
+				label: "Sekerr Range (Mtelo)",
+				code: "40147_SkrrRng(Mtl)"
+			}, {
+				label: "Shimba Hills",
+				code: "40147_ShmbHlls"
+			}, {
+				label: "Tana River Delta",
+				code: "40147_TnRvrDlt"
+			}, {
+				label: "Tana River Mid Section",
+				code: "40147_TnRvrMddlSctn"
+			}, {
+				label: "Tsavo Parks",
+				code: "40147_TsvEstndWst"
+			}, {
+				label: "Turkwel River",
+				code: "40147_TrkwlRvr"
+			}],
+			regions: ['country', 'pentad', 'province', 'group', 'qdgc'],
+			regions_opt: ['all', 'country', 'pentad', 'province', 'group', 'qdgc'],
 			countries: ['kenya', 'southafrica', 'nigeria', 'botswana', 'namibia', 'zimbabwe', 'lesotho', 'swaziland', 'mozambique'],
 			structure: {},
 			species: [],
-			lurl: 'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmFmbnVzcyIsImEiOiIzMVE1dnc0In0.3FNMKIlQ_afYktqki-6m0g',//'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+			lurl: 'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmFmbnVzcyIsImEiOiIzMVE1dnc0In0.3FNMKIlQ_afYktqki-6m0g', //'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 			bounds: L.latLngBounds([
 				[5.061499, 33.106787],
 				[-5.316542, 42.232546]
 			]),
-			displayMarkerPentad: false,
+			buttonDisplayMarkerPentad: false,
 			marker_pentad: {
-				position: { lat: -1.219482, lng: 38.953153 },
+				position: {
+					lat: -1.219482,
+					lng: 38.953153
+				},
 			},
 			geojson: {
 				"type": "FeatureCollection",
@@ -102,14 +247,19 @@ window.onload = function () {
 		},
 		mounted() {
 			var self = this;
-			$.getJSON('http://kbm.raphaelnussbaumer.com/structure.json', 
-			function (json) {
-				self.structure = json;
-			})
-			$.getJSON('http://kbm.raphaelnussbaumer.com/species.json', 
-			function (json) {
-				self.species = json.map(l => { return { code: l[0], label: l[1] } });
-			})
+			$.getJSON('structure.json',
+				function (json) {
+					self.structure = json;
+				})
+			$.getJSON('species.json',
+				function (json) {
+					self.species = json.map(l => {
+						return {
+							code: l[0],
+							label: l[1]
+						}
+					});
+				})
 			/*cookies_input.forEach(function(s){
 				var v = getCookieValue(s);
 				if (!(v === "")){
@@ -118,83 +268,90 @@ window.onload = function () {
 			})*/
 		},
 		computed: {
-			rectangle_pentad: function(){
+			displayMarkerPentad : function(){ return this.buttonDisplayMarkerPentad && this.regionType=="pentad" },
+			rectangle_pentad: function () {
 				var lng = this.marker_pentad.position.lng;
 				var lat = this.marker_pentad.position.lat;
-				
-				this.regionPentad = latlng2pentad(lat,lng)
-				
-				var  lat0 = Math.floor((lat+0.000001)/res)*res
-				var  lng0 = Math.floor((lng+0.000001)/res)*res
-				return [[lat0, lng0], [lat0+res, lng0+res]]
+
+				this.regionPentad = latlng2pentad(lat, lng)
+
+				var lat0 = Math.floor((lat + 0.000001) / res) * res
+				var lng0 = Math.floor((lng + 0.000001) / res) * res
+				return [
+					[lat0, lng0],
+					[lat0 + res, lng0 + res]
+				]
 			},
-			base_opts: function(){
+			base_opts: function () {
 				return Object.keys(this.structure)
 			},
 			clayer: function () {
-				if (Object.keys(this.structure).length==0){
+				if (Object.keys(this.structure).length == 0) {
 					return []
-				} else{
-					var tmp =this.structure[this.base];
+				} else {
+					var tmp = this.structure[this.base];
 					return (typeof tmp == 'undefined') ? [] : tmp
 				}
 			}
 		},
 		methods: {
-			url: function(){
+			url: function () {
 				var url = ""
-				jQuery('#url-group').children().each( function(){
-					if( jQuery(this).hasClass('url-val') ) {
-						if (jQuery(this).val()=="all"){
-							url = url.slice(0,url.length-1);
-						} else{
-							url += String(jQuery(this).val()).replace('null','')
+				jQuery('#url-group').children().each(function () {
+					if (jQuery(this).hasClass('url-val')) {
+						if (jQuery(this).val() == "all") {
+							url = url.slice(0, url.length - 1);
+						} else {
+							url += String(jQuery(this).val()).replace('null', '')
 						}
-					} else if (jQuery(this).hasClass('url-species')){
+					} else if (jQuery(this).hasClass('url-species')) {
 						url += Array.isArray(app.speciesCode) ? app.speciesCode.join(',') : app.speciesCode
-						
-					} else if (jQuery(this).hasClass('url-innertext')){
+
+					} else if (jQuery(this).hasClass('url-innertext')) {
 						url += this.innerText
-					} 
+					}
 				})
-				return url.replace(/ /g,'').replace(/\r?\n|\r/g,'').replace('null').replace(/\/$/, "")
+				return url.replace(/ /g, '').replace(/\r?\n|\r/g, '').replace('null').replace(/\/$/, "")
 				/*cookies_input.forEach(function(s){
 					document.cookie= s + "=" + encodeURIComponent(app[s]) + ";" + expires + ";path=/";
 				})*/
 			},
 			exportURL: function (type, e) {
 				console.log(app.url())
-				if (type=="toJSON"){
-					window.open( app.url()+'?format=JSON');
-				} else if (type == 'toCSV'){
-					window.open( app.url()+'?format=CSV');
-				}else if (type == 'togeoJSON'){
-					window.open( app.url()+'?format=geoJSON');
-				} else if (type == 'map'){
-					jQuery(e.target).html('<i class="fa fa-spinner fa-spin"></i> Loading')
-					jQuery.getJSON( app.url()+'?format=geoJSON' , function(data){
-						
-						if (data.type == "FeatureCollection"){
+				if (type == "toJSON") {
+					window.open(app.url() + '?format=JSON');
+				} else if (type == 'toCSV') {
+					window.open(app.url() + '?format=CSV');
+				} else if (type == 'togeoJSON') {
+					window.open(app.url() + '?format=geoJSON');
+				}
+			},
+			exportMap: function (e) {
+				console.log(app.url())
+				jQuery(e.target).html('<i class="fa fa-spinner fa-spin"></i> Loading')
+				jQuery.getJSON(app.url() + '?format=geoJSON', function (data) {
+
+						if (data.type == "FeatureCollection") {
 							app.geojson = data
-						} else if ( data.meta.call.includes("cards/species/0/") ){
+						} else if (data.meta.call.includes("cards/species/0/")) {
 							app.geojson = {
 								"type": "FeatureCollection",
-								"features": data.data.cards.reduce((acc,c) => {
-									
-									var id = acc.map(a=>a.properties.pentad).indexOf(c.Pentad)
-									
+								"features": data.data.cards.reduce((acc, c) => {
+
+									var id = acc.map(a => a.properties.pentad).indexOf(c.Pentad)
+
 									pp = {
-										CardNo:c.CardNo,
-										StartDate:c.StartDate,
+										CardNo: c.CardNo,
+										StartDate: c.StartDate,
 										ObserverNo: c.ObserverNo,
-										TotalHours:c.TotalHours,
+										TotalHours: c.TotalHours,
 										TotalSpp: c.TotalSpp,
 										Common_name: c.records[0].Common_species + ' ' + c.records[0].Common_group,
-										Scientific_name: c.records[0].Genus+' '+c.records[0].Species,
+										Scientific_name: c.records[0].Genus + ' ' + c.records[0].Species,
 										Sequence: c.records[0].Sequence
 									}
-									
-									if (id != -1 ){
+
+									if (id != -1) {
 										acc[id].properties.card.push(pp)
 									} else {
 										latlng = pentad2latlng(c.Pentad)
@@ -204,43 +361,45 @@ window.onload = function () {
 											"properties": {
 												"pentad": c.Pentad,
 												"name": c.Pentad,
-												"popupContent":  c.Pentad,
+												"popupContent": c.Pentad,
 												"card": [
 													pp
 												]
 											},
 											"geometry": {
 												"type": "Polygon",
-												"coordinates": [[
-													[latlng[1], latlng[0]],
-													[latlng[1], latlng[0]+res],
-													[latlng[1]+res, latlng[0]+res],
-													[latlng[1]+res, latlng[0]],
-													[latlng[1], latlng[0]]
-												]]
+												"coordinates": [
+													[
+														[latlng[1], latlng[0]],
+														[latlng[1], latlng[0] + res],
+														[latlng[1] + res, latlng[0] + res],
+														[latlng[1] + res, latlng[0]],
+														[latlng[1], latlng[0]]
+													]
+												]
 											}
 										})
 									}
 									return acc
-								},[])
+								}, [])
 							}
-						} else if ( data.meta.call.includes("card/full/0/") ){
+						} else if (data.meta.call.includes("card/full/0/")) {
 							app.geojson = {
 								"type": "FeatureCollection",
-								"features": data.data.cards.reduce((acc,c) => {
-									
-									var id = acc.map(a=>a.properties.pentad).indexOf(c.Pentad)
-									
+								"features": data.data.cards.reduce((acc, c) => {
+
+									var id = acc.map(a => a.properties.pentad).indexOf(c.Pentad)
+
 									pp = {
-										CardNo:c.CardNo,
-										StartDate:c.StartDate,
+										CardNo: c.CardNo,
+										StartDate: c.StartDate,
 										ObserverNo: c.ObserverNo,
-										TotalHours:c.TotalHours,
+										TotalHours: c.TotalHours,
 										TotalSpp: c.TotalSpp,
-										Common_name: c.records.map(r=>r.Common_species + ' ' + r.Common_group).join(', ')
+										Common_name: c.records.map(r => r.Common_species + ' ' + r.Common_group).join(', ')
 									}
-									
-									if (id != -1 ){
+
+									if (id != -1) {
 										acc[id].properties.card.push(pp)
 									} else {
 										latlng = pentad2latlng(c.Pentad)
@@ -255,33 +414,34 @@ window.onload = function () {
 											},
 											"geometry": {
 												"type": "Polygon",
-												"coordinates": [[
-													[latlng[1], latlng[0]],
-													[latlng[1], latlng[0]+res],
-													[latlng[1]+res, latlng[0]+res],
-													[latlng[1]+res, latlng[0]],
-													[latlng[1], latlng[0]]
-												]]
+												"coordinates": [
+													[
+														[latlng[1], latlng[0]],
+														[latlng[1], latlng[0] + res],
+														[latlng[1] + res, latlng[0] + res],
+														[latlng[1] + res, latlng[0]],
+														[latlng[1], latlng[0]]
+													]
+												]
 											}
 										})
 									}
 									return acc
-								},[])
+								}, [])
 							}
 						}
-						
+
 						jQuery(e.target).html('<i class="fas fa-globe"></i> Map')
-						
+
 					})
-					.fail(function( jqxhr, textStatus, error ) {
+					.fail(function (jqxhr, textStatus, error) {
 						jQuery(e.target).html('<i class="fas fa-globe"></i> Map')
 						var err = textStatus + ", " + error;
-						alert( "Request Failed: " + err +'<br>Check that the url is valid and that the key is added.');
+						alert("Request Failed: " + err + '<br>Check that the url is valid and that the key is added.');
 					});
-				}
 			}
 		}
 	})
-	
-	
-} 
+
+
+}
